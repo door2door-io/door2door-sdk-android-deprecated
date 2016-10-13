88a17c95cf0180493de8b01c23e9dca0dd53c0e1
Zlatko Stamatov <Zlatko.Stamatov@gmail.com>
2016-10-13 14:50:10 +0200

    MA-11: Setup of static analysis tools.

--- lib/src/main/java/io/door2door/demandsdksample/base/HelloWorld.java
+++ lib/src/main/java/io/door2door/demandsdksample/base/HelloWorld.java
@@ -1,8 +1,16 @@
 package io.door2door.demandsdksample.base;
 
+/**
+ * Example class.
+ */
 public class HelloWorld {
 
+    /**
+     * Example method.
+     * @return example value
+     */
     public String getHelloWorld() {
         return "Hello World";
     }
+
 }
